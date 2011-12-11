11591fb748132c7ede1b0c84ba65474a05793e78
Ted Kaemming <ted@kaemming.com>
2011-12-10 21:03:57 -0800

    add packaging options for ignore patterns and no preprocess/postprocess, command logging

--- src/staticutils/management/commands/packagestatic.py
+++ src/staticutils/management/commands/packagestatic.py
@@ -1,8 +1,10 @@
 import fnmatch
+from optparse import make_option
 from django.contrib.staticfiles.finders import get_finders
 from django.core.files.base import ContentFile
 from django.core.files.storage import get_storage_class
 from django.core.management.base import NoArgsCommand
+from django.utils.encoding import smart_str
 from django.utils.importlib import import_module
 from staticutils import settings
 
@@ -15,14 +17,38 @@ def process(name, content, processors):
 
 class Command(NoArgsCommand):
     can_import_settings = True
+    option_list = NoArgsCommand.option_list + (
+        make_option('-i', '--ignore', action='append', default=[],
+            dest='ignore_patterns', metavar='PATTERN',
+            help="Ignore files or directories matching this glob-style "
+                "pattern. Use multiple times to ignore more."),
+        make_option('--no-default-ignore', action='store_false',
+            dest='use_default_ignore_patterns', default=True,
+            help="Don't ignore the common private glob-style patterns 'CVS', "
+                "'.*' and '*~'."),
+        make_option('--no-preprocess',
+            action='store_false', dest='preprocess', default=True,
+            help="Do NOT preprocess package contents."),
+        make_option('--no-postprocess',
+            action='store_false', dest='postprocess', default=True,
+            help="Do NOT postprocess package contents."),
+    )
 
-    def handle_noargs(self, *args, **kwargs):
+    def handle_noargs(self, **options):
+        self.verbosity = int(options.get('verbosity', 1))
+        self.preprocess = options['preprocess']
+        self.postprocess = options['postprocess']
+        ignore_patterns = options['ignore_patterns']
+        if options['use_default_ignore_patterns']:
+            ignore_patterns += ['CVS', '.*', '*~']
+        self.ignore_patterns = list(set(ignore_patterns))
+
         storage = get_storage_class(settings.STATIC_PACKAGE_STORAGE)
         destination = storage(**settings.STATIC_PACKAGE_STORAGE_OPTIONS)
 
         files = {}
         for finder in get_finders():
-            files.update(dict(finder.list(ignore_patterns='') or []))
+            files.update(dict(finder.list(ignore_patterns=self.ignore_patterns) or []))
 
         for package, patterns in settings.STATIC_PACKAGES.items():
             names = []
@@ -34,10 +60,30 @@ class Command(NoArgsCommand):
                     matches = fnmatch.filter(files.keys(), pattern)
                 names.extend(matches)
 
+            names = set(names) # don't include the same file twice in the same package
+            file_count = len(names)
+            self.log('Creating static package: %s (%s %s)' % (package,
+                file_count, file_count == 1 and "file" or "files"), level=1)
+
             contents = []
             for name in names:
-                contents.append(process(name, files[name].open(name).read(),
-                    settings.STATIC_PACKAGE_PREPROCESSORS))
+                self.log("   - %s" % name, level=2)
+                content = files[name].open(name).read()
+                if self.preprocess:
+                    content = process(name, content,
+                        settings.STATIC_PACKAGE_PREPROCESSORS)
+                contents.append(content)
 
-            destination.save(package, ContentFile(process(package,
-                "\n".join(contents), settings.STATIC_PACKAGE_POSTPROCESSORS)))
+            content = "\n".join(contents)
+            if self.postprocess:
+                content = process(package, content,
+                    settings.STATIC_PACKAGE_POSTPROCESSORS)
+
+            destination.save(package, ContentFile(content))
+
+    def log(self, msg, level=2):
+        msg = smart_str(msg)
+        if not msg.endswith("\n"):
+            msg += "\n"
+        if self.verbosity >= level:
+            self.stdout.write(msg)
